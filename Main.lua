393db75285b2de5b48d33a926a60e25e3c58f246
MiniLuminary <142038307+MiniLuminary@users.noreply.github.com>
2023-08-12 18:57:42 +0300

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,4 +1,5 @@
 local KeySystemUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/MaGiXxScripter0/keysystemv2api/master/ui/xrer_mstudio45.lua"))()
+local module = loadstring(game:HttpGet("https://raw.githubusercontent.com/MiniLuminary/LuminaryLuauHub/main/Module.lua"))()
 KeySystemUI.New({
     ApplicationName = "LuminaryHub",
     Name = "❤ Luminary",
@@ -8,6 +9,7 @@ KeySystemUI.New({
 repeat task.wait() until KeySystemUI.Finished() or KeySystemUI.Closed
 if KeySystemUI.Finished() and KeySystemUI.Closed == false then
     print("Key verified, can load script")
+    module.FoundGame()
 else
     print("Player closed the GUI.")
 end
